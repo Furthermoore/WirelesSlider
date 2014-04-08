6d508acedc349be4ca11e1fe230796ccbf1941fd
Dan Moore <dhmoore@berklee.edu>
2014-04-08 00:42:59 -0400

    first commit

---
+++ WirelesSlider/WirelesSlider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WirelesSlider
+//
+//  Created by Dan Moore on 4/8/14.
+//  Copyright (c) 2014 Dan Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WirelesSlider/WirelesSlider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WirelesSlider
+//
+//  Created by Dan Moore on 4/8/14.
+//  Copyright (c) 2014 Dan Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
